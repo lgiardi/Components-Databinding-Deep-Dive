d2ccc8bfc4dffc39e4087ffc3aa14ac2a87fa093
lgiardi <luca.giardina1980@gmail.com>
2020-11-10 15:12:58 +0100

    5 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  serverElements = [];
+  serverElements = [{type :'bluprint' , name:'test', content :'7server'}];
 }
